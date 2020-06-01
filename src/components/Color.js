f275c11a8a773960aa6cc2b0c6a8d7e1edc6f3d4
XINGJIAN WANG <wangxingjian94@gmail.com>
2020-06-01 14:12:27 -0400

    changed back

--- src/components/Color.js
+++ src/components/Color.js
@@ -15,7 +15,7 @@ class Color extends React.Component {
                   {this.props.color.colorName} 
                  </div>    
                <div className="button" onClick={this.handleClick.bind(this)}>
-               <a href="" id="Copy"> COPYA </a>    
+               <a href="" id="Copy"> COPY </a>    
              </div>
             </div>
         );
